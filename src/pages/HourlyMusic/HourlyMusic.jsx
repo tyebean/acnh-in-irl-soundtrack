466bdb59c2dbcaa39d8a53e58d4351cf9b2814ac
tyebean <flower.webster@gmail.com>
2023-01-31 16:02:15 -0500

    stub up hourly snippets

--- src/pages/HourlyMusic/HourlyMusic.jsx
+++ src/pages/HourlyMusic/HourlyMusic.jsx
@@ -1,3 +1,11 @@
 // plays music hourly based on device time
 // automatically muted
 // button to unmute
+
+import React from 'react'
+
+export default function HourlyMusic() {
+  return (
+    <div>HourlyMusic</div>
+  )
+}
