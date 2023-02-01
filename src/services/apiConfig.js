d1527fff91c45acc6f70e54c9e6c49346b2106ce
tyebean <flower.webster@gmail.com>
2023-01-31 20:03:12 -0500

    add weather api url

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -3,6 +3,12 @@ import axios from 'axios';
 //connect animal crossing new horizons api url
 const acnhUrl = 'https://acnhapi.com/v1a/'
 
+const weatherUrl = 'https://api.weather.gov/'
+
 export const acnhApi = axios.create({
   baseURL: acnhUrl
+})
+
+export const weatherApi = axios.create({
+  baseURL: weatherUrl
 })
