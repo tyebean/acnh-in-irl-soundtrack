c760310a5fdb6738d529666202bc811f71a59458
tyebean <flower.webster@gmail.com>
2023-02-01 00:35:30 -0500

    add background images

---
+++ src/components/Background.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Background() {
+
+  return (
+    <>
+      <div class="bg">
+        <img src="public/backgrounds/rainy.gif" alt=""></img>
+      </div>
+    </>
+  )
+}
