4f6b72d34826d86673e466206b38fc98b80edd5d
tyebean <flower.webster@gmail.com>
2023-01-31 20:02:01 -0500

    move coord getter code

--- src/pages/HourlyMusic/HourlyMusic.jsx
+++ src/pages/HourlyMusic/HourlyMusic.jsx
@@ -1,22 +1,19 @@
 import { useState } from 'react'
 import GetHourlyMusic from '../../components/GetHourlyMusic.jsx'
 import GetRealLifeHour from '../../components/GetRealLifeHour.jsx'
-// import GetRealLifeWeather from '../../components/GetRealLifeWeather.jsx'
+import GetRealLifeWeather from '../../components/GetRealLifeWeather.jsx'
 
 export default function HourlyMusic() {
 
   const [hourly, setHourly] = useState([])
   const [music, setMusic] = useState([])
 
-  navigator.geolocation.getCurrentPosition((position)=> {
-    const p=position.coords;
-    console.log(p.latitude,p.longitude);
-})
   
   return (
     <div>
       <GetHourlyMusic hourly={setHourly} music={setMusic}/>
       <GetRealLifeHour  />
+      <GetRealLifeWeather  />
       <h1>Hello! The time it is _time_ _date_ and it is it is _degrees_ degrees in your_location.</h1>
       <h1>We are playing background_music_name_ to reflect hour _hour_ and the _weather_ weather conditions in animal crossing new horizons </h1>
     </div>
